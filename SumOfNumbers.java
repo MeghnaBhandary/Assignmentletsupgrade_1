15e363b71cd5fa46e23d0bd3236e3aeee6ce08f9
MeghnaBhandary <88940120+MeghnaBhandary@users.noreply.github.com>
2021-08-14 21:49:06 +0530

    Add files via upload
    Day2 Java assignment submission

---
+++ SumOfNumbers.java
@@ -0,0 +1,11 @@
+//Day2
+public class SumOfNumbers
+{
+	public static void main(String[] args)
+    {
+        int X = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int sum = x + y;
+        System.out.println("Sum of two numbers is" +sum);
+    }
+}
